9c1cbae158d7e39d439156ca1af5967e786c0da1
Shaine21 <158239412+Shaine21@users.noreply.github.com>
2024-08-02 22:02:04 +0800

    Add files via upload

---
+++ little_leaf_guardian/play.js
@@ -0,0 +1,42 @@
+class play extends Phaser.Scene {
+    constructor ()
+    {
+        super({ key: 'play' });
+    }
+
+    preload() {
+        this.load.image('playImg','assets/scenes/play.jpg');
+
+    }
+
+    create () {
+
+
+        this.add.image(0, 0, 'play').setOrigin(0, 0);
+        
+        this.add.text(0,580, 'Press Space to continue', { font: '24px Courier', fill: '#000000' });
+
+        console.log("*** play scene");
+        this.scene.bringToTop("play");
+     
+       // Add image and detect spacebar keypress
+       this.add.image(0, 0, 'playImg').setOrigin(0, 0);
+     
+       // Check for spacebar or any key here
+       let spaceDown = this.input.keyboard.addKey("SPACE");
+     
+       // On spacebar event, call the world scene
+       spaceDown.on("down", function () {
+       console.log("Jump to level_01_game scene");
+       window.heart = 3;
+       window.key = 0;
+       window.goldseed = 0
+       
+       this.scene.start("level_01_game");
+         },
+         this
+       );
+       
+       }
+     
+     }
---
+++ little_leaf_guardian/storyLine.js
@@ -0,0 +1,42 @@
+class storyline extends Phaser.Scene {
+    constructor ()
+    {
+        super({ key: 'storyline' });
+    }
+
+    preload() {
+        this.load.image('storylineImg','assets/scenes/storyline.jpg');
+
+    }
+
+    create () {
+
+
+        this.add.image(0, 0, 'storyline').setOrigin(0, 0);
+        
+        this.add.text(0,580, 'Press Enter to continue', { font: '24px Courier', fill: '#000000' });
+
+        console.log("*** storyline scene");
+        this.scene.bringToTop("storyline");
+     
+       // Add image and detect spacebar keypress
+       this.add.image(0, 0, 'storylineImg').setOrigin(0, 0);
+     
+       // Check for spacebar or any key here
+       let spaceDown = this.input.keyboard.addKey("SPACE");
+     
+       // On spacebar event, call the world scene
+       spaceDown.on("down", function () {
+       console.log("Jump to instruction1 scene");
+       window.heart = 3;
+       window.key = 0;
+       window.goldseed = 0
+       
+       this.scene.start("instruction1");
+         },
+         this
+       );
+       
+       }
+     
+     }
